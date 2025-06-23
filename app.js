823a506f407f7f8dafdc6c92b13c3564b334fb14
MarkiianPerliak <perliak2012@gmail.com>
2025-06-23 20:30:34 +0300

    done

--- app.js
+++ app.js
@@ -3,9 +3,27 @@
 // адресою https://api.github.com/users та перегляньте отримані дані щодо
 // користувачів.
 
+function createListUsers(data) {
+    const usersList = document.querySelector(".users__list");
+    const htmlElement = data.map(user => {
+        const elementOfLi = `
+        <li class="users__item">
+        <div>
+            <h2 class="users__nickname">${user.login}</h2>
+            <p class="users__node-id">Node-ID - ${user.node_id}</p>
+            <img class="users__picture" src="${user.avatar_url}" alt="picture">
+        </div>
+        </li>
+        `
+        return elementOfLi
+    })
+    const htmlElementString = htmlElement.join('')
+    usersList.innerHTML = htmlElementString
+}
+
 function getUsersAPI() {
     return fetch("https://api.github.com/users")
 }
 
-getUsersAPI().then((result) => result.json()).then((resultdata) => console.log(resultdata))
+getUsersAPI().then((result) => result.json()).then((resultdata) => createListUsers(resultdata))
 
